c26c422e92f86c1c06e9638f4b995e08a42c38c6
mukhtar89 <muhammad.28.1989@gmail.com>
2015-02-22 20:43:45 -0800

    Commit1

---
+++ Fract_serial/mutil.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+
+#include "mutil.h"
+
+void maccess(int *A, int *R, int a1, int a2, int b1, int b2)
+{
+	for (int a = a1; a <= a2; a++)
+		for (int b = b1; b <= b2; b++)
+			R[((a - a1)*(b2 - b1) + (b - b1))] = A[(a*(b2 - b1) + b)];
+}
+
+void maccess(int *A, int *R, int a1, int a2, int b1, int b2, int c)
+{
+	for (int a = a1; a <= a2; a++)
+		for (int b = b1; b <= b2; b++)
+			R[((a - a1)*(b2 - b1) + (b - b1))] = A[(a*(b2 - b1) + b)];
+}
+void msave(int *A, int *R, int a1, int a2, int b1, int b2, int c)
+{
+	for (int a = a1; a <= a2; a++)
+		for (int b = b1; b <= b2; b++)
+			R[((a - a1)*(b2 - b1) + (b - b1))] = A[(a*(b2 - b1) + b)];
+}
+
+void msave(int *A, int *R, int a1, int a2, int b1, int b2, int c)
+{
+	for (int a = a1; a <= a2; a++)
+		for (int b = b1; b <= b2; b++)
+			R[((a - a1)*(b2 - b1) + (b - b1))] = A[(a*(b2 - b1) + b)];
+}
+
+int avg_matrix(int *R)
+{
+	int Rsize = sizeof(R);
+	int average = 0;
+	for (int x = 0; x<Rsize; x++)
+		for (int y = 0; y<Rsize; y++)
+			average += R[(x*Rsize) + y];
+	average /= ((int)pow(Rsize, 2));
+}
+
---
+++ Fract_serial/mutil.h
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+
+
+void maccess(int *A, int *R, int a1, int a2, int b1, int b2);     //access A, save into R
+
+void maccess(int *A, int *R, int a1, int a2, int b1, int b2, int c);    //access A, save into R
+
+void msave(int *A, int *R, int a1, int a2, int b1, int b2, int c);    //access A, save into R
+
+void msave(int *A, int *R, int a1, int a2, int b1, int b2, int c);    //access A, save into R
+
+int avg_matrix(int *R);
+
+void rotmat(int *D, int *rot);   //rotate matrix D, save it into rot
+
+void fliph(int *D, int *rot);   //rotate matrix D, save it into rot
+
+void flipv(int *D, int *rot);   //rotate matrix D, save it into rot
+
+void transpose(int *D, int *rot);   //rotate matrix D, save it into rot
